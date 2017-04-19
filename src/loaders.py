ea02b3e487a668647dd36767d3abf963be5c4a32
Aaron Thomas <ftk2ne@gmail.com>
2017-04-19 01:45:36 -0400

    Initial commit

---
+++ src/loaders.py
@@ -0,0 +1,14 @@
+def load_entity(filepath):
+	'''
+	loads an entity from a file
+	'''
+	f = f.open(filepath)
+
+def load_item(filepath):
+	pass
+
+def load_room(filepath):
+	pass
+
+def load_thing(filepath):#this hsouldnt exist tbh
+	pass
